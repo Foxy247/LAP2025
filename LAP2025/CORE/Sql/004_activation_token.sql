467e73fe3ad88cd49c17a5827772027d0d311375
Emanuel Kölbl <83544812+Foxy247@users.noreply.github.com>
2026-01-29 13:00:46 +0100

    Delete LAP2025 directory

--- LAP2025/CORE/Sql/004_activation_token.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE activation_token
-(
-    id      INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    user_id INT UNIQUE,
-    token   VARCHAR(510),
-    FOREIGN KEY (user_id) REFERENCES users (id)
-);
--- LAP2025/CORE/Sql/005_orders.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE orders
-(
-    id      INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    user_id INT,
-    date    DATE,
-    status  tinyint(1),
-    FOREIGN KEY (user_id) REFERENCES users (id)
-);
